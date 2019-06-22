1ed683fd6ffbc39b7d3fc1d909d9aada92b3f6c6
LORRAINE BAYNOSA <you@example.com>
2019-06-22 07:22:55 -0700

    need to match with pic and name

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// This data source hold the array of information on friends.
 // ===============================================================================
 
 var friends = require("../data/friends");
@@ -10,30 +10,35 @@ var friends = require("../data/friends");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // (ex: localhost:PORT/api/friends... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     return res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have friends or not.
     // req.body is available since we're using the body parsing middleware
-      friends.push(req.body);
-      // for (var i = 0; i <friends.scores.length; i++) {
-    //   var friendsScore = friends.scores[i];
-    //   for (var j= 0; j , newFriend.scores.length; j++){
-    //     var newFriendScore = userScores[j];
-    //   }
-    //   // We calculate the difference between the scores and sum them into the totalDifference
-    //   totalDifference += Math.abs(parseInt(friendsScore) - parseInt(newFriendScore));
-      // res.json(newFriend);
+    var compatibility = [];
+    for (var i = 0; i < friends.length; i++) {
+      var scores = [];
+      for (var j = 0; j < friends[i].scores.length; j++) {
+        scores.push((Math.abs(parseInt(friends[i].scores[j]) - parseInt(req.body.scores[j]))));
+      }
+      console.log("Scores:" + scores);
+      var totalDifference = scores.reduce((total, match)=> total + match, 0)
+      console.log("Total Difference:" + totalDifference);
+      compatibility.push(totalDifference);
+    }
+    console.log("Compatibility:" + compatibility);
+    friends.push(req.body);
   });
+}
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
@@ -44,4 +49,3 @@ module.exports = function(app) {
 
   //   res.json({ ok: true });
   // });
-};
