ac527b233d058a6bac364ee6e6b853b7766d322a
LORRAINE BAYNOSA <you@example.com>
2019-06-22 16:01:13 -0700

    saving ROC

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -15,6 +15,54 @@ var friends =
         4,
         1
       ]
+  },
+  {
+    name:"Richard",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        3,
+        2,
+        1,
+        4,
+        5,
+        4,
+        2,
+        5,
+        3,
+        2
+      ]
+  },
+  {
+    name:"Jacob",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        3,
+        1,
+        4,
+        3,
+        5,
+        1,
+        2,
+        1,
+        1,
+        1
+      ]
+  },
+  {
+    name:"Mark",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        1,
+        5,
+        4,
+        1
+      ]
   }
 ]
 
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -13,8 +13,8 @@ var friends = require("../data/friends");
 module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/friends... they are shown a JSON of the data in the table)
+  // In each of the below cases when a user visits the api/friends link
+  // (ex: localhost:PORT/api/friends... they are shown a JSON of the friends array with a get request
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function (req, res) {
@@ -24,6 +24,14 @@ module.exports = function (app) {
   app.post("/api/friends", function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have friends or not.
     // req.body is available since we're using the body parsing middleware
+    var userInput = req.body;
+    userInput = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: req.body.scores
+    }
+
+    var totalDifference = 0;
     var compatibility = [];
     for (var i = 0; i < friends.length; i++) {
       var scores = [];
@@ -31,21 +39,19 @@ module.exports = function (app) {
         scores.push((Math.abs(parseInt(friends[i].scores[j]) - parseInt(req.body.scores[j]))));
       }
       console.log("Scores:" + scores);
-      var totalDifference = scores.reduce((total, match)=> total + match, 0)
-      console.log("Total Difference:" + totalDifference);
+      var totalDifference = scores.reduce((total, match) => total + match, 0)
+      console.log("Sum of Absolute Differences:" + totalDifference);
       compatibility.push(totalDifference);
     }
+    // Match with least amount of difference is the best match.
+    var bestMatch = Math.min.apply(null, compatibility);
+    console.log("Best Match: " + bestMatch);
+    // The indexOf() method returns the first index at which a given element 
+    // can be found in the array.
+    var matchIndex = compatibility.indexOf(bestMatch)
+    console.log(matchIndex);
     console.log("Compatibility:" + compatibility);
     friends.push(req.body);
   });
 }
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-
-  // app.post("/api/clear", function(req, res) {
-  //   // Empty out the arrays of data
-  //   friends.length = [];
-
-  //   res.json({ ok: true });
-  // });
+  
